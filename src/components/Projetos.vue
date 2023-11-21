56af9e40e23b4297260ba2873ba3eaeb6bb7ad21
David Wilson de Jesus <80333501+davidwjdev@users.noreply.github.com>
2023-11-20 22:40:55 -0300

    (Fix): Ajuste layout projetos

--- src/components/Projetos.vue
+++ src/components/Projetos.vue
@@ -5,14 +5,29 @@
     >
         <h2 class="md:text-3xl sm:text-2xl font-bold m-10">PROJETOS</h2>
         <div class="text-base m-10">
-            <div class="">
-                <span
-                    class="z-10 top-20 md:left-1/3 sm:right-0 center relative font-bold"
-                    >Em desenvolvimento...</span
-                >
-                <div
-                    class="bg-neutral-700 rounded-lg h-36 flex justify-center items-center blur-md z-0"
-                ></div>
+            <div class="w-full">
+                <span class="center font-bold">Em desenvolvimento...</span>
+
+                <ul class="flex flex-wrap justify-center blur-md mt-10">
+                    <li class="flex flex-col">
+                        <div
+                            class="h-36 w-36 m-3 mr-2 p-5 rounded-lg flex items-center bg-neutral-700"
+                        ></div>
+                        <span class="text-center blur-none">EM BREVE</span>
+                    </li>
+                    <li class="flex flex-col">
+                        <div
+                            class="h-36 w-36 m-3 mr-2 p-5 rounded-lg flex items-center bg-neutral-700"
+                        ></div>
+                        <span class="text-center">EM BREVE</span>
+                    </li>
+                    <li class="flex flex-col">
+                        <div
+                            class="h-36 w-36 m-3 mr-2 p-5 rounded-lg flex items-center bg-neutral-700"
+                        ></div>
+                        <span class="text-center">EM BREVE</span>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
